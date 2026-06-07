97c36dfc793af0b5348e6ed1056acf10accf2f91
lijimathew123 <160114591+liji-elkanio@users.noreply.github.com>
2026-06-07 20:30:25 +0530

    page reloding problem fixed

--- frontend/smart-travel-frontend/src/services/api.jsx
+++ frontend/smart-travel-frontend/src/services/api.jsx
@@ -10,8 +10,10 @@ const api = axios.create({
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) {
-    config.headers.Authorization = `Token ${token}`;
+    // simplejwt expects: Authorization: Bearer <token>
+    config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
 
